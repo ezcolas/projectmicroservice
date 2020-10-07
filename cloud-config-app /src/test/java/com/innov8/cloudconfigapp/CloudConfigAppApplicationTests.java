57953c487badb4407ed14084fec68ba6cd2f4349
ezcolas <ezechielcolas@gmail.com>
2020-10-06 19:55:56 -0500

    Spring cloud config with local repository

---
+++ cloud-config-app /src/test/java/com/innov8/cloudconfigapp/CloudConfigAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innov8.cloudconfigapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
